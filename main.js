2dd43777a190c5996a5c7d10e0e99342da370349
SabaShavidze <sabashavidze@gmail.com>
2019-03-15 13:08:57 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(".chat-header-primary").click(function(){
+        $(".chat-header-primary").css({
+            "flex":"0.17",
+            "transition":"flex 0.8s"
+        });
+        $(".chat-welcome-secondary").css("display","none");
+        $(".chat-welcome-primary").css({
+            "margin-left":"10px",
+            "font-size":"18px",
+            "transition":"margin-left 0.8s"
+        }); 
+    })
+})
+
+$(document).ready(function(){
+    $(".chat-header-primary").click(function(){
+        var a = $(".chat-welcome-primary").css("margin-left");
+        if(a==="10px"){
+            $(".chat-header-primary").css({
+            "flex":"0.8",
+            "transition":"flex 0.8s"
+        });
+        $(".chat-welcome-secondary").css({"display":"block","transition":"0.8s"});
+        $(".chat-welcome-primary").css({
+            "margin-left":"117px",
+            "font-size":"20px",
+            "transition":"margin-left 0.8s"
+        }); 
+        }
+    })
+})
+$(document).ready(function(){
+    $(".chat-close").click(function(){
+        $(".live-chat-form").css("display","none");
+    })
+})
+
+$(document).ready(function(){
+    $(".messenger-icon").click(function(){
+        $(".live-chat-form").css("display","block");
+        console.log("samdkasmd")
+    })
+})
+       
