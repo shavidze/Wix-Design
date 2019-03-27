7c96d95ed9639dc3f9a489c8aa8beb2c2ca200f6
user57 <user57@DESKTOP-OKBP5JC>
2019-03-27 14:39:41 +0400

    toggle bar

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-    $(".chat-header-primary").click(function() { $('.chat-header-primary').toggleClass('clicked') })
+$(document).ready(function () {
+  $(".chat-header-primary").click(function () { $('.chat-header-primary').toggleClass('clicked') })
 
-    $(".chat-close").click(function() {
-      $(".live-chat-form").css("display", "none");
-    });
-  
-    $(".messenger-icon").click(function() {
-      $(".live-chat-form").css({ display: "block" });
-    });
+  $(".chat-close").click(function () {
+    $(".live-chat-form").css("display", "none");
+  });
+
+  $(".messenger-icon").click(function () {
+    $(".live-chat-form").css({ display: "block" });
+  });
+  $(".menu-toggle").click(function () {
+    $("#main-nav").toggleClass('clicked', 2000);
+  })
 })
 
 
- 
